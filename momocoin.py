1afe9b112d3b198cc76709fdfa5d413bd9735204
Mehul Oswal <mehuloswal21@gmail.com>
2021-07-15 23:31:26 +0530

    Added Transactions List to the Block for MINING

--- momocoin.py
+++ momocoin.py
@@ -90,6 +90,9 @@ class BlockChain:
 #creating a web app
 app=Flask(__name__)
 
+#Creating an address for the node on Port 5000
+node_address = str(uuid4()).replace('-','')
+
 #creating a blockchain
 blockchain = BlockChain()
 
@@ -100,13 +103,15 @@ def mine_block():
     previous_proof = previous_block['proof']
     proof = blockchain.proof_of_work(previous_proof)
     previous_hash = blockchain.hash(previous_block)
+    blockchain.add_transaction(sender = node_address, receiver= 'Mehul', amount=1)
     block = blockchain.create_block(proof, previous_hash)
     response = {
         'message': 'Congratulations, you just mined a block on my customized blockchain!',
         'index' : block['index'],
         'timestamp' : block['timestamp'],
         'proof' : block['proof'],
-        'previous_hash': block['previous_hash']
+        'previous_hash': block['previous_hash'],
+        'transactions': block['transactions']
     }
     return jsonify(response), 200
 
