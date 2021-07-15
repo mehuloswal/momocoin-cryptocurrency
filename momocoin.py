7c3ce5eb9ac0a0e4a1bec39bfb5586358a6fa0dc
Mehul Oswal <mehuloswal21@gmail.com>
2021-07-15 13:00:35 +0530

    Replacing with longest chain on the network

--- momocoin.py
+++ momocoin.py
@@ -17,6 +17,7 @@ class BlockChain:
         self.chain = []
         self.transactions = []  #mempool
         self.create_block(proof = 1, previous_hash = '0' )
+        self.nodes = set()
         
     def create_block(self, proof, previous_hash):
         block = {'index': len(self.chain)+1, 'timestamp': str(datetime.datetime.now()), 'proof': proof, 'previous_hash': previous_hash, 'transactions': self.transactions}
@@ -62,6 +63,27 @@ class BlockChain:
         self.transactions.append({'sender': sender, 'receiver':receiver, 'amount': amount})
         previous_block = self.get_previous_block()
         return previous_block['index'] + 1
+
+    def add_node(self, address_of_node):
+        parsed_url = urlparse(address_of_node)
+        self.nodes.add(parsed_url.netloc)
+
+    def replace_chain(self):
+        network = self.nodes
+        longest_chain = None
+        max_length = len(self.chain)
+        for node in network:
+            response = requests.get(f'http://{node}/get-chain')
+            if response.status_code == 200:
+                new_length = response.json()['length']
+                chain = response.json()['chain']
+                if new_length > max_length and self.is_chain_valid(chain):
+                    max_length = new_length
+                    longest_chain = chain
+        if longest_chain:
+            self.chain = longest_chain
+            return True
+        return False                
 
 # Part-2 Mining our Blockchain
 
